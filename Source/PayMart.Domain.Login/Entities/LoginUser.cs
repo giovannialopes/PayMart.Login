772d90f1cfec396f83e59c835473cf4974800b87
Giovanni Lopes <giovannialopes.dev@gmail.com>
2024-08-21 01:31:42 -0300

    feature/ instanciando o entity framework para a criação do DB + criação da entidade LoginUser.

---
+++ Source/PayMart.Domain.Login/Entities/LoginUser.cs
@@ -0,0 +1,10 @@
+﻿namespace PayMart.Domain.Login.Entities;
+
+public class LoginUser
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Password { get; set; } = "";
+    public string Email { get; set; } = "";
+
+}
---
+++ Source/PayMart.Infrastructure.Login/DataAcess/DbLogin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PayMart.Domain.Login.Entities;
+
+namespace PayMart.Infrastructure.Login.DataAcess;
+
+public class DbLogin : DbContext
+{
+    public DbLogin(DbContextOptions options) : base(options) { }
+
+    public DbSet<LoginUser> Tb_User { get; set; }
+
+}
